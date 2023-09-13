e1fb8144a85b2da4ae2459745771daa954b3747b
Qlove Iman <imanqlove@gamil.com>
2023-09-13 19:10:12 +0600

    added Modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import classes from 'Cart.module.css';
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
 
 const Cart = (props) => {
   const cartItems = (
@@ -11,16 +12,19 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
-      <div>
+      <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
